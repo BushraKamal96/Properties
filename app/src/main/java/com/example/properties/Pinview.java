f9ef35fa646d1ae890ac52756ed8e5d4ec02e30e
bushrakamal96 <sweetybushra10@hotmail.com>
2020-10-04 19:26:51 +0500

    test

---
+++ app/src/main/java/com/example/properties/Pinview.java
@@ -0,0 +1,14 @@
+package com.example.properties;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pinview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pinview);
+    }
+}
